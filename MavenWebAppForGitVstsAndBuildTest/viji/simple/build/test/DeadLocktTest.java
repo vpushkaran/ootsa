c8db95947bfaaae00fbdef640b8f63ebb6fb5c68
Viji <vpushkaran@gmail.com>
2019-01-03 16:34:10 -0600

    test commit to master

---
+++ MavenWebAppForGitVstsAndBuildTest/viji/simple/build/test/DeadLocktTest.java
@@ -0,0 +1,18 @@
+package viji.simple.build.test;
+
+public class DeadLocktTest {
+	
+//  static {
+	  //    Thread t = new Thread(Deadlock::new);
+	  //    t.start();
+	  //    try {
+	  //      t.join();
+	  //    } catch (InterruptedException e) {}
+	  //  }
+
+	   Deadlock() {}
+
+		 public static void main(String[] args) {
+			 System.out.println("Test Run");
+		 }
+}
